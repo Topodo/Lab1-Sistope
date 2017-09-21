ba28b32b446c0c2db7e6312603c0cb7805bf6117
Topodo <ariel.tirado@usach.cl>
2017-09-20 22:57:54 -0300

    Coordinación de procesos hijos terminada

--- hola.c
+++ hola.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char const *argv[]) {
-  printf("Hola mundo, soy un proceso hijo y me ejecute con execv\n");
+  printf("Hola mundo, soy un proceso hijo y leo desde el byte %lu hasta el byte %lu\n", strtol(argv[1], NULL, 10), strtol(argv[2], NULL, 10));
   return 0;
 }
